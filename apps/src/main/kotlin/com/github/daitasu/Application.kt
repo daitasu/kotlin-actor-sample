f7f268b47bc468829e8bc30392b31ac2a0c1bcbc
daitasu <ifctp.dai.monara@gmail.com>
2025-01-02 23:26:47 +0900

    refactor: Change to using coroutine

--- apps/src/main/kotlin/com/github/daitasu/Application.kt
+++ apps/src/main/kotlin/com/github/daitasu/Application.kt
@@ -1,97 +1,57 @@
 package com.github.daitasu
 
-import java.util.*
-import java.util.concurrent.ConcurrentLinkedQueue
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlin.system.measureTimeMillis
 
 /**
- * キューに格納されるメッセージ用インターフェース
+ * キューに追加されるメッセージクラス
  */
-interface Message {
-  fun clone(): Message
-}
+data class GreetingMessage(val message: String)
 
 /**
- * Messageを実装した具体的なメッセージクラス。
+ * Channel を利用したアクターモデルっぽいサンプル
  */
-data class GreetingMessage(var message: String) : Message {
-  override fun clone(): Message = copy()
-}
+class CoroutineGreetingActor(
+    private val actorName: String,
+    private val scope: CoroutineScope
+    ) {
+    private val channel = Channel<GreetingMessage>(capacity = 100)
 
-/**
- * Actor の抽象クラス
- *
- * - Runnable を実装してスレッドで動かせるようにする
- * - メッセージを保持するキューを用意する
- * - tell() によってメッセージを受け取り、キューに格納する
- * - run() 内でキューからメッセージを取り出し、onReceive() で処理を委譲する
- */
-abstract class AbstractActor : Runnable {
-    private val thread = Thread(this)
-    private val messages: Queue<Message> = ConcurrentLinkedQueue()
-
-    /**
-     * キューにメッセージを格納し、必要に応じてスレッドを起動する
-     * NEW, TERRMINATED ならスレッドを起動/再起動する
-     */
-    fun tell(message: Message) {
-        messages.add(message.clone())
-
-        when (thread.state) {
-            Thread.State.NEW, Thread.State.TERMINATED -> thread.start()
-            else -> {
-              // 既に動作中なら何もしない
+    init {
+        GlobalScope.launch {
+            for (msg in channel) {
+                process(msg)
             }
         }
     }
 
-    /**
-     * スレッドのメインループ
-     * キューからメッセージを取り出して onReceive() で処理させる
-     */
-    override fun run() {
-        while (messages.isNotEmpty()) {
-            val msg = messages.poll() ?: continue
-            onReceive(msg)
-        }
+    suspend fun tell(message: GreetingMessage) {
+        channel.send(message)
     }
 
-    /**
-     * 受け取ったメッセージをどのように処理するかは継承先で実装する
-     */
-    protected abstract fun onReceive(msg: Message)
-}
-
-/**
- * GreetingMessage を受け取ってコンソールに表示するだけのシンプルな Actor
- */
-class GreetingActor(
-  private val actorName: String
-) : AbstractActor() {
-
-  override fun onReceive(msg: Message) {
-        if (msg is GreetingMessage) {
-          println("[$actorName] Received: ${msg.message}")
-
-          Thread.sleep(5000)
-
-          println("[$actorName] Done: ${msg.message}")
-
-        } else {
-            println("GreetingActor received unknown message: $msg")
-        }
+    private suspend fun process(msg: GreetingMessage) {
+        println("[$actorName] Received: ${msg.message}")
+        delay(2000)
+        println("[$actorName] Done: ${msg.message}")
     }
 }
+
 
 fun main() {
+  runBlocking {
     // アクターを生成
-    val actor1 = GreetingActor(actorName = "Actor1")
-    val actor2 = GreetingActor(actorName = "Actor2")
+    val actor1 = CoroutineGreetingActor("Actor1", this)
+    val actor2 = CoroutineGreetingActor("Actor2", this)
 
     // メッセージを送信
-    actor1.tell(GreetingMessage("Hello from main() #1"))
-    actor1.tell(GreetingMessage("Hello from main() #2"))
+    actor1.tell(GreetingMessage("Hello #1"))
+    actor2.tell(GreetingMessage("Hello #2"))
+    actor1.tell(GreetingMessage("Hello #3"))
+    actor2.tell(GreetingMessage("Hello #4"))
 
-    actor2.tell(GreetingMessage("Another greeting #1"))
-    actor2.tell(GreetingMessage("Another greeting #2"))
+    println("All messages sent from main")
+  }
+
+  Thread.sleep(10000)
 }
